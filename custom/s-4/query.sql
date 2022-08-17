f23396cf54fdbc1d153fe5e341d796ec14cbb279
Josua Rochat <rochat.josua@gmail.com>
2022-08-17 08:50:41 +0200

    Intial commit

--- custom/s-4/query.sql
+++ custom/s-4/query.sql
@@ -1,4 +1,14 @@
+
+/* SafetyInfo QUERY */
 SELECT c.customerId, GETDATE() as UpdatedAt, GETDATE() as CreatedAt
 FROM OrderInfo c
-INNER JOIN 
-ON c.
+WHERE c.OrderType='purchase'
+
+
+/* Sendable Data Extension QUERY */
+SELECT s.customerId, c.email, e.EventDate
+FROM SafetyInfo s
+INNER JOIN ContactInfo c
+ON s.customerId= s.ContactId
+INNER JOIN EventsInfo e
+ON e.EventId=o.EventId /* we assume that the eventid is available from the Order object */
